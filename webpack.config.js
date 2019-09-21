895b4de1dfa415ef1d46a884b25a6c414db9692c
Matt Bise <mbise1993@gmail.com>
2019-09-21 12:00:41 -0500

    build submodule

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,13 @@
 const path = require('path');
 
 module.exports = {
+  entry: {
+    index: './src/index.ts',
+    'test-utilities': './src/test-utilities/index.ts'
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
+    filename: '[name].js',
   },
   resolve: {
     modules: [path.resolve(__dirname, 'src'), 'node_modules'],
